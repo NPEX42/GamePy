116560d795df648b9c842842b6ec1f7b8d33339c
npex42 <npex42@gmail.com>
2020-09-11 14:46:56 +0100

    Add Engine & Game

---
+++ Engine.py
@@ -0,0 +1,15 @@
+import pygame
+
+pygame.init();
+
+class WindowUtils:
+	def __init__(self):
+		self.window = 
+	def OpenWindow(self, width, height, title):
+		pass
+
+	def UpdateWindow(self):
+		pass
+
+	def CloseWindow(self):
+		pass
---
+++ Game.py
@@ -0,0 +1,3 @@
+from Engine import *
+
+OpenWindow(1080,720,"Hello PyGame!")
