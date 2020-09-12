19369d23d7677919c7d1ef6439b437edacbc3c59
npex42 <npex42@gmail.com>
2020-09-12 20:51:50 +0100

    Added Background Coloring & Image Loading

--- Engine.py
+++ Engine.py
@@ -5,10 +5,11 @@ pygame.init();
 class Window:
 	def __init__(self):
 		self.clock = pygame.time.Clock();
+		self.frame = None;
 		pass
 
 	def OpenWindow(self, width, height, title):
-		pygame.display.set_mode((width, height));
+		self.frame = pygame.display.set_mode((width, height));
 		pygame.display.set_caption(title);
 
 	def UpdateWindow(self):
@@ -18,17 +19,33 @@ class Window:
 				return False;
 		self.clock.tick(60);
 		return True
-		pass
 
 	def CloseWindow(self):
 		pygame.quit();
 		pass
 
+	def BlitToWindow(self, item):
+		self.frame.blit(item, (0,0));
+
 class Renderer:
 	def __init__(self):
-		
+		self.surface = None;
+		self.frame = None;
 
-	def 
+	def OpenSurface(self ,width, height, frame):
+		self.surface = pygame.Surface((width, height), pygame.OPENGL);
+		self.frame = frame;
+
+	def Background(self,color):
+		self.surface.fill(color);
+		self.WriteToScreen();
+
+	def DrawImage(self, image, pos):
+		self.surface.blit(image,pos);
+		self.WriteToScreen();
+
+	def WriteToScreen(self):
+		self.frame.BlitToWindow(self.surface);
 
 
 class GameState:
@@ -40,3 +57,7 @@ class GameState:
 
 	def Update(self, ts):
 		pass
+
+
+def LoadImage(path):
+	return pygame.image.load(path);
--- Game.py
+++ Game.py
@@ -1,10 +1,15 @@
 from Engine import *
 
 mainWindow = Window();
+renderer = Renderer();
 
-mainWindow.OpenWindow(480,480,"Hello PyGame!")
+mainWindow.OpenWindow(480,480,"Hello PyGame!");
+renderer.OpenSurface(480, 480, mainWindow);
+
+splash = LoadImage("Splash_480.png");
 
 while mainWindow.UpdateWindow():
-	pass
+	renderer.Background((255,255,255));
+	renderer.DrawImage(splash, (0,0));
 
 mainWindow.CloseWindow()
